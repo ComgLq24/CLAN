5d0bedb513706f3386c657cd24e038969be62578
ComgLq24 <ComgLqWork@outlook.com>
2021-11-17 16:14:36 +0800

    add viewer

--- train_viewer.py
+++ train_viewer.py
@@ -6,24 +6,26 @@ import numpy as np
 
 
 class Viewer(object):
-    def __init__(self):
-        self.vis = visdom.Visdom(server='10.16.170.78', port=8097)
-        self.loss_data = None
-        self.loss_win = None
+    def __init__(self, server_ip, port):
+        self.vis = visdom.Visdom(server=server_ip, port=port)
+        self.loss_data = {'loss_seg': None, 'loss_adv': None, 'loss_weight': None, 'loss_D_s': None,
+                          'loss_D_t': None}
+        self.loss_win = {'loss_seg': None, 'loss_adv': None, 'loss_weight': None, 'loss_D_s': None,
+                         'loss_D_t': None}
         if not self.vis.check_connection():
             cmd = sys.executable + '-m visdom server'
             Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 
-    def plot_current_loss(self, iteration, loss):
-        if self.loss_data is None:
-            self.loss_data = {'X': [iteration], 'Y': [loss]}
-            self.loss_win = self.vis.line(
-                X=np.array(self.loss_data['X']),
-                Y=np.array(self.loss_data['Y']),
+    def plot_current_loss(self, iteration, loss, type):
+        if self.loss_data[type] is None:
+            self.loss_data[type] = {'X': [iteration], 'Y': [loss]}
+            self.loss_win[type] = self.vis.line(
+                X=np.array(self.loss_data[type]['X']),
+                Y=np.array(self.loss_data[type]['Y']),
                 opts={
                     'markers': True,
                     'markersize': 6,
-                    'title': 'training loss',
+                    'title': type,
                     'xlabel': 'iteration',
                     'ylabel': 'loss'
                 }
@@ -32,6 +34,6 @@ class Viewer(object):
         self.vis.line(
             X=np.array([iteration]),
             Y=np.array([loss]),
-            win=self.loss_win,
+            win=self.loss_win[type],
             update='append'
         )
